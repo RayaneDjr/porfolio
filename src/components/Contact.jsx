257f943df3ea7cac1babdee422cd29aec7491201
RayaneDjr <rayane.djaffar@outlook.fr>
2024-06-11 18:29:58 +0200

    responsive done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,11 +4,11 @@ const Contact = () => {
   return (
     <div
       name='contact'
-      className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+      className='w-full min-h-screen bg-[#0a192f] flex justify-center items-center p-4'>
       <form
         method='POST'
         action='https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c'
-        className='flex flex-col max-w-[600px] w-full'>
+        className='flex flex-col max-w-[600px] w-full mt-20'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>
             Contact
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,10 +14,10 @@ const Skills = () => {
   return (
     <div
       name='skills'
-      className='w-full min-h-screen bg-[#0a192f] text-gray-300'>
+      className='w-full md:h-1 min-h-screen bg-[#0a192f] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div>
+        <div className='mt-20'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>
             Skills
           </p>
